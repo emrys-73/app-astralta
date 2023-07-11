ac28c4dbd551f3c2dcac3a25df9410aa86d984f3
Adrian Montoya <adrian.montoya@isaraerospace.com>
2023-07-11 23:03:23 +0200

    Streaming GPT-4

--- src/routes/my/ai/talk/+page.svelte
+++ src/routes/my/ai/talk/+page.svelte
@@ -1,8 +1,13 @@
-<script>
+<script lang="ts">
   // @ts-nocheck
-  
-    import { Configuration, OpenAIApi } from "openai";
     import { onMount } from "svelte"; 
+    import { useChat } from "ai/svelte";
+
+    const { messages, handleSubmit, input } = useChat({
+      api: "/chat",
+    });
+
+
     let message = ""
   
     /**
@@ -13,23 +18,7 @@
     let preventSubmit = false;
     const MAX_ROWS = 6;
   
-    const configuration = new Configuration({
-        apiKey: "ahskf",
-      });
-  
-    const openai = new OpenAIApi(configuration);
-  
-    const chat = async ()  => {
-      const completion = await openai.createChatCompletion({
-        model: "gpt-4",
-        messages:  [{"role": "system", "content": "You are Astralta, my own customised AI. Your task is to serve me and reply to me in the style of Jarvis from Iron Man. My name is Adrian but you will call me Sir. "}, {role: "user", content: message}],
-      })
-      
-      // @ts-ignore
-      response = completion.data.choices[0].message.content;
-      // @ts-ignore
-      console.log(completion.data);
-    }
+ 
   
     onMount(() => {
        const input = document.querySelector("textarea[name=message]"); 
@@ -49,6 +38,24 @@
   
   </script>
 
+
+<div class="min-h-[400px] min-w-[800px] m-6 bg-black bg-opacity-30 backdrop-blur-md rounded-xl items-center px-20 content-center flex flex-row">
+  <ul>
+    {#each $messages as message}
+      <li>
+        {message.role} says... {message.content}
+      </li>
+
+    {/each}
+  </ul>
+
+
+  <form on:submit={handleSubmit}>
+    <input bind:value={$input}/>
+    <button type="submit">Send</button>
+  </form>
+</div>
+<!-- 
 <div class="text-center text-true-white text-md  md:max-w-[500px] xl:max-w-[800px] bg-black max-h-[600px] rounded-xl bg-opacity-40 backdrop-blur-sm">
   {#if response} 
   <p class=" w-full p-2 px-4">
@@ -60,13 +67,14 @@
     </p>
   {/if}
 </div>
+ -->
+
 
 <div class="w-full sm:max-w-[600px] lg:max-w-[800px] grid grid-cols-12 absolute bottom-2 xl:bottom-4 content-center items-center justify-center">
   <div class="col-span-2">
     <a href="https://astralta.com">
       <div class="hover:-translate-y-0.5 transition ease-in-out h-[40px] lg:ml-10">
         <button class="btn bg-true-black bg-opacity-50 hover:bg-opacity-20 hover:bg-gray-300 rounded-full text-true-white font-semibold btn-sm md:text-md md:w-16 border-none normal-case drop-shadow-2xl h-[40px] ml-4 apple-btn w-[50px]">
-          <!-- Learn more -->
           <img src="/logo_white.png" alt="cta" class="h-[30px] w-[30px]"/>
         </button>
       </div>
@@ -77,8 +85,7 @@
   </div>
   <div class="col-span-2">
     <div class="hover:-translate-y-0.5 transition ease-in-out">
-      <button class="btn bg-true-black  text-xl bg-opacity-50 hover:bg-opacity-20 hover:bg-gray-300 rounded-full text-true-white font-semibold btn-sm md:text-md md:w-16 border-none normal-case drop-shadow-2xl h-[40px] ml-4 apple-btn w-[50px]" on:click={chat} type="submit">
-        <!-- Learn more -->
+      <button class="btn bg-true-black  text-xl bg-opacity-50 hover:bg-opacity-20 hover:bg-gray-300 rounded-full text-true-white font-semibold btn-sm md:text-md md:w-16 border-none normal-case drop-shadow-2xl h-[40px] ml-4 apple-btn w-[50px]" type="submit">
         ➤
       </button>
     </div>
