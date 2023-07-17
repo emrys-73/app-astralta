6ee7c8b33af6ccf0ae539305ac2d8289f6c79ccb
Adrian Montoya <adrian.montoya@isaraerospace.com>
2023-07-17 23:40:23 +0200

    enabled preformated responses

--- src/routes/chat/+server.ts
+++ src/routes/chat/+server.ts
@@ -19,6 +19,11 @@ const config = new Configuration({
 const openai = new OpenAIApi(config);
 
 
+// const get_current_weather = (locaton, ) => {
+//     return {}
+// }
+
+
 export const POST: RequestHandler = async ({ request })  => {
 
     let xp;
@@ -31,9 +36,28 @@ export const POST: RequestHandler = async ({ request })  => {
 
     const response = await openai.createChatCompletion({
         model: 'gpt-3.5-turbo',
+        // model: 'gpt-3.5-turbo-0613',
         // model: 'gpt-4',
         stream: true,
-        messages: augmentedMessages
+        messages: augmentedMessages,
+        // functions: [
+        //     {
+        //         "name": "get_current_weather",
+        //         "description": "Get the current weather in a given location",
+        //         "parameters": {
+        //             "type": "object",
+        //             "properties": {
+        //                 "location": {
+        //                     "type": "string",
+        //                     "description": "The city and state, e.g. San Francisco, CA",
+        //                 },
+        //                 "unit": {"type": "string", "enum": ["celsius", "fahrenheit"]},
+        //             },
+        //             "required": ["location"],
+        //         },
+        //     }
+        // ],
+        // function_call:"auto",
     })
 
     const stream = OpenAIStream(response)
--- src/routes/my/ai/+page.svelte
+++ src/routes/my/ai/+page.svelte
@@ -38,8 +38,11 @@
         api: "/chat",
       });
 
-      let message = "";
-      const MAX_ROWS = 6;
+      
+      let message;
+
+      input.subscribe((value) => message = value)
+      const MAX_ROWS = 10;
 
 
       $: rows = message.length > 0 ? Math.min(Math.ceil(message.length / 80), MAX_ROWS) : 1;
@@ -88,24 +91,18 @@
   {#if talking} 
 
   <div class="flex justify-center h-full relative">
-    <div class="min-w-[350px] sm:max-w-[700px] md:max-w-[80] lg:max-w-[900] xl:max-w-[1000px] m-6 rounded-xl flex justify-center items-center bg-black bg-opacity-0 mb-20 overflow-y-auto h-[500px]">
+    <div class="min-w-[350px] sm:max-w-[700px] md:max-w-[80] lg:max-w-[900] xl:max-w-[1000px] m-6 rounded-xl flex justify-center items-center bg-black bg-opacity-0 mb-20 overflow-y-auto">
       <div class="row-span-5 overflow-auto">
+        <!-- <pre class="w-full overflow-auto p-2 px-4 ">{message}
+        </pre> -->
         <ul>
           {#each $messages as message}
           <li>
             {#if message.role == 'user'}
-            <div class="text-left text-true-white text-md bg-true-white rounded-xl bg-opacity-5 backdrop-blur-md my-2">
-    
-              <p class="w-full p-2 px-4 overflow-auto">
-                {message.content}
-              </p>
-            </div>
+            <div class="text-left text-true-white text-md bg-true-white rounded-xl bg-opacity-0 backdrop-blur-xl my-2 drop-shadow-xl"><pre class="w-full p-2 px-4 overflow-auto whitespace-pre-wrap">{message.content}<pre/></div>
             {:else}
-            <div class="text-left text-true-white text-md bg-black rounded-xl bg-opacity-60 backdrop-blur-md my-2">
-              <p class="w-full p-2 px-4 overflow-auto" innerHTML="{formatMessageContent(message.content)}">
-                {message.content}
-              </p>
-            </div>
+            <div class="text-left text-true-white text-md bg-black rounded-xl bg-opacity-60 backdrop-blur-xl my-2 drop-shadow-xl">
+              <pre class="w-full p-2 px-4 overflow-y-auto whitespace-pre-wrap">{message.content}</pre></div>
             {/if}
           </li>
           {/each}
