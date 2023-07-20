7577fc6d9712412397479459fffc1070755913e9
Adrian Montoya <adrian.montoya@isaraerospace.com>
2023-07-20 23:38:58 +0200

    GPT-3.5-Turbo update

--- src/routes/chat/+server.ts
+++ src/routes/chat/+server.ts
@@ -42,7 +42,7 @@ export const POST: RequestHandler = async ({ request })  => {
         return new StreamingTextResponse(stream)
     } else {
         const response = await openai.createChatCompletion({
-            model: 'gpt-4',
+            model: 'gpt-3.5-turbo',
             stream: true,
             messages: messages,
         })
