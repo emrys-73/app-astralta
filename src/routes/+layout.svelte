976a8e9ba62008bb747620dcaa4f4af33de9efd4
Adrian Montoya <adrian.montoya@isaraerospace.com>
2023-07-26 06:58:31 +0200

    Analytics update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,12 +9,12 @@
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
 	import { LeftDrawer } from '$lib/components';
-	export let data; // to accept that data property
 	import { getImageURL } from "$lib/utils";
     import { drawerOpen, darkMode } from '../stores';
     import { dev } from '$app/environment';
     import { inject } from '@vercel/analytics';
     inject({ mode: dev ? 'development' : 'production' });
+	export let data; // to accept that data property
 
     let darkModeState = false;
     darkMode.subscribe((value) => darkModeState = value);
