89913c65214b2b591a54432f1acd1b4fce40ced2
Adrian Montoya <adrian.montoya@isaraerospace.com>
2023-07-27 12:12:53 +0200

    DarkMode Card attempt

---
+++ src/lib/components/RespCard.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { darkMode } from "../../stores";
+
+</script>
+
+
+<div class="{$darkMode ? 'bg-black bg-opacity-20' : 'bg-white bg-opacity-10'} rounded-2xl w-[5rem] h-[5rem]">
+    <slot/>
+</div>
