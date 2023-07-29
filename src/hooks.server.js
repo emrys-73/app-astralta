d470f5bbc4206bbd192519bb8b8f9ee30106f708
Adrian Montoya <adrian.montoya@isaraerospace.com>
2023-07-29 18:26:00 +0200

    secure cookie off

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -38,7 +38,7 @@ export const handle = async ({ event, resolve }) => {
 
     const response = await resolve(event)
 
-    response.headers.set('set-cookie', event.locals.pb.authStore.exportToCookie({ secure: true }))
+    response.headers.set('set-cookie', event.locals.pb.authStore.exportToCookie({ secure: false }))
 
     return response
 }
