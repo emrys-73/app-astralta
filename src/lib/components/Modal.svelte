92192b02374572df642c7229f1c3d0b78ed5e032
Adrian Montoya <adrian.montoya@isaraerospace.com>
2023-07-29 21:27:32 +0200

    Repair Modal

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,25 @@
+<script>
+    // @ts-nocheck
+    
+        export let label;
+        export let checked;
+    </script>
+    
+    <label for={label} class="justify-center">
+        <slot name="trigger" />
+    </label>
+    
+    <input type="checkbox" id={label} class="modal-toggle" {checked} />
+    <label for={label} class="modal modal-bottom sm:modal-middle  items-center bg-black px-4">
+        <label for="" class="modal-box bg-black bg-opacity-40 drop-shadow-xl backdrop-blur-md text-true-white">
+            <div class="font-bold text-xl text-center text-true-white mt-1">
+                <slot name="heading" />
+            </div>
+            <div class="my-4 items-center text-true-white">
+                <slot />
+            </div>
+            <!-- <div class="justify-center text-true-white">
+                <slot name="actions" />
+            </div> -->
+        </label>
+    </label>
--- src/lib/components/index.js
+++ src/lib/components/index.js
@@ -4,5 +4,6 @@ import LiveCard from './LiveCard.svelte';
 import Input from './Input.svelte';
 import AltaCard from './AltaCard.svelte';
 import Button from './Button.svelte'
+import Modal from "./Modal.svelte";
 
-export { Drawer, InfoModal, LiveCard, Input, AltaCard, Button }
+export { Drawer, InfoModal, LiveCard, Input, AltaCard, Button, Modal }
