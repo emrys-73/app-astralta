6e13e973af2e05f0f22c996f215aae78a89ea463
Adrian Montoya <montoya@astralta.com>
2023-08-09 16:28:09 +0200

    Design bug fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -84,7 +84,7 @@
     {:else}
   
     <a href="/train">
-      <div class="my-4 flex flex-row text-center items-center justify-center gap-2  backdrop-blur-md {$darkMode ? 'bg-black bg-opacity-80' : 'bg-black bg-opacity-30'} drop-shadow-xl text-true-white p-4 min-w-[350px] hover:bg-true-white hover:bg-opacity-5 hover:cursor-pointer rounded-2xl transition duration-700 ease-in-out">
+      <div class="my-4 flex flex-row text-center items-center justify-center gap-2 backdrop-blur-md {$darkMode ? 'bg-black bg-opacity-80' : 'bg-black bg-opacity-30'} drop-shadow-xl text-true-white p-4 min-w-[350px] hover:bg-true-white hover:cursor-pointer rounded-2xl transition duration-700 ease-in-out">
         <div>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
@@ -202,7 +202,7 @@
         <LiveCard class="">
           <div class="absolute left-2 bottom-4">
             <a href="/">
-              <button on:click={showConfirmDelete(chat.id)} class="{$darkMode ? 'bg-black bg-opacity-40' : 'bg-black opacity-5' } px-4 py-2 rounded-2xl hover:bg-red-600 hover:bg-opacity-100">
+              <button on:click={showConfirmDelete(chat.id)} class="{$darkMode ? 'bg-black bg-opacity-40' : 'bg-black bg-opacity-5' } px-4 py-2 rounded-2xl hover:bg-red-600 hover:bg-opacity-100">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                 </svg>
@@ -225,7 +225,7 @@
             {/if}
             <div class="absolute right-2 bottom-4">
               <a href="/">
-                <button on:click={toggleEditForm(chat.id)} class="{$darkMode ? 'bg-black bg-opacity-40' : 'bg-black opacity-5' } px-4 py-2 rounded-2xl hover:bg-true-white hover:bg-opacity-10">
+                <button on:click={toggleEditForm(chat.id)} class="{$darkMode ? 'bg-black bg-opacity-40' : 'bg-black bg-opacity-10' } px-4 py-2 rounded-2xl hover:bg-true-white hover:bg-opacity-10">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
                   </svg>
