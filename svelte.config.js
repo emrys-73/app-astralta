8b3d6f3a5fc06b7753fa8833501c291a53e31a7f
Adrian Montoya <montoya@astralta.com>
2023-08-25 03:42:54 +0200

    switch to edge

--- svelte.config.js
+++ svelte.config.js
@@ -11,11 +11,16 @@ const config = {
 	vitePlugin: {
 		inspector: true,   
 	},
+	// kit: {
+	// 	// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+	// 	// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+	// 	// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+	// 	adapter: adapter()
+	// }
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({ runtime: 'edge' }),
+
+		inlineStyleThreshold: 5000
 	}
 };
 export default config;
